d3ac891db3af37f08b3381c473e8a9d2526597be
Poorna Ranasinghe <onlinepoorna@gmail.com>
2022-09-16 19:00:10 +0530

    admin stats back-end

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,9 +6,11 @@ import {
   deleteUser,
   UpdateUser,
   getAllUsers,
+  showStats,
 } from "../controllers/userController.js";
 
 router.route("/").get(getAllUsers);
 router.route("/:id").patch(UpdateUser).delete(deleteUser);
+router.route("/stats").get(showStats);
 
 export default router;
